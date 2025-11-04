b2aa78331b53b8e9fc5c9d88b24136deea66e0f7
Tamim Mohammed Kabir <yolosoul22@outlook.com>
2025-11-04 12:34:37 -0500

    remove delay before finish

--- tb_toUpper.v
+++ tb_toUpper.v
@@ -9,27 +9,27 @@ module tb_toUpper;
 
     initial begin
         $display("Decimal\tBinary\tAscii_IN\tAscii_OUT");
-        in = 8'd40; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd72; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd183; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd131; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd124; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd20; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd235; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd97; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd65; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd122; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd71; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd109; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd146; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd48; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd207; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd58; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd123; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd148; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
-        in = 8'd127; #30;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd40; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd72; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd183; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd131; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd124; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd20; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd235; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd97; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd65; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd122; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd71; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd109; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd146; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd48; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd207; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd58; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd123; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd148; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
+        in = 8'd127; #26;  $display("%d\t%b\t%c\t%c", in, in, in, out);
 
-        #10 $finish;
+        $finish;
     end
 
     initial begin
